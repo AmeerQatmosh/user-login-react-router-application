9914729f837eb6beaef699be6ee2a09abcb3de97
Ameer Qatmosh <ameerqsh@gmail.com>
2023-05-29 10:22:58 +0300

    edit-routes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./Pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/user-login-react-router-application">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
